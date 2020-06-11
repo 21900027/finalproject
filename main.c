e2d3116ce5c74b448d2583bdc446b52fc2e0a2bf
21900027 <21900027@handong.edu>
2020-06-11 15:35:18 +0100

    make main.c

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <studio.h>
+#include "header.h"
+
+int main(void) {
+	  int choosenum;
+	    
+	    print page
+	    firstpage();
+	    secondpage();
+
+	    while(1){
+		       //choose the number
+			printf("\n\n=============================================\n");
+			printf("\nChoose the number that you want to do ");
+			scanf("%d",&choosenum);
+
+			//1 make file
+			if (choosenum == 1){
+			printf("you choose the make the file\n");
+			printf("\n\n=============================================\n");
+			makefile();
+			printf("\n\n=============================================\n");
+			}
+
+			//2 show list
+			else if(choosenum == 2){
+			printf("you choose the show list\n");
+			showlist();
+			}
+
+			 //3 read diary
+			 else if(choosenum == 3){
+			 printf("you choose the read diary");
+			 printf("\n\n=============================================\n");
+			 openfile();
+			 } 
+			
+			 //4 exit
+			 else if(choosenum == 4){
+			 printf("you choose the exit");
+			 byepage();
+			 exit(0);
+			 }
+			
+			else{
+            		 printf("You put the wrong number!");
+    			}	
+	     	      }
+
+			  return 0;
+			}
+
