c5f4724ec5da62813d985332a599bfbe9c4a59b5
21900027 <21900027@handong.edu>
2020-06-11 15:23:04 +0100

    make header.h

---
+++ header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+void firstpage();
+void secondpage();
+void byepage();
+void makefile();
+void savelist();
+void showlist();
+void openfile();
+~                                                                                                                       ~                                                                                                                       ~                                                                                                                       ~                            
